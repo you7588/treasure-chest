7815cbe1168ab8f46cafa75a82107cba2e8ed53c
you7588 <yo7588@gmail.com>
2017-08-02 22:33:59 +0800

    add admin users page

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,11 @@ Rails.application.routes.draw do
 
   resources :events
 
+
   namespace :admin do
     root "events#index"
     resources :events
+      resources :users
   end
 
   root "events#index"
