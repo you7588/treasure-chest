49466aafc32883599bb01a01fd59a0c412fa8739
you7588 <yo7588@gmail.com>
2017-08-02 19:02:17 +0800

    use to_s to change time pattern

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,5 @@ module RailsRecipes
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+Time::DATE_FORMATS.merge!(:default => '%Y/%m/%d %I:%M %p', :ymd => '%Y/%m/%d')
