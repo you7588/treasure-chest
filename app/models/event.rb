4ffd0d30a824f9b0840d6bea6043f7007e238340
you7588 <yo7588@gmail.com>
2017-08-03 11:30:43 +0800

    add nested to UI --1-to-many

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
 
+  has_many :tickets, :dependent => :destroy
+  accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
+
   STATUS = ["draft", "public", "private"]
   validates_inclusion_of :status, :in => STATUS
 
