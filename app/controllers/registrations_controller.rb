a28ef6e998ab41d87093144cb7d410447162e5a2
you7588 <yo7588@gmail.com>
2017-08-05 22:06:01 +0800

    add validate to each step

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,6 +9,7 @@ class RegistrationsController < ApplicationController
     @registration.ticket = @event.tickets.find( params[:registration][:ticket_id])
     @registration.status = "pending"
     @registration.user = current_user
+    @registration.current_step = 1
 
     if @registration.save
       redirect_to step2_event_registration_path(@event, @registration)
@@ -23,6 +24,7 @@ class RegistrationsController < ApplicationController
 
   def step1_update
     @registration = @event.registrations.find_by_uuid(params[:id])
+    @registration.current_step = 1
 
     if @registration.update(registration_params)
       redirect_to step2_event_registration_path(@event, @registration)
@@ -37,6 +39,7 @@ class RegistrationsController < ApplicationController
 
   def step2_update
     @registration = @event.registrations.find_by_uuid(params[:id])
+    @registration.current_step = 2
 
     if @registration.update(registration_params)
       redirect_to step3_event_registration_path(@event, @registration)
@@ -52,6 +55,7 @@ class RegistrationsController < ApplicationController
   def step3_update
     @registration = @event.registrations.find_by_uuid(params[:id])
     @registration.status = "confirmed"
+    @registration.current_step = 3
 
     if @registration.update(registration_params)
       flash[:notice] = "报名成功"
