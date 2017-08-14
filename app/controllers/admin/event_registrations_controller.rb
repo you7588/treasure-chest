7a936abe9e7136a95aec71ab61ef210403d60fa7
you7588 <yo7588@gmail.com>
2017-08-14 16:46:08 +0800

    select time

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -20,6 +20,15 @@ class Admin::EventRegistrationsController < ApplicationController
      @registrations = @registrations.by_ticket(params[:ticket_ids])
    end
 
+      if params[:start_on].present?
+     @registrations = @registrations.where( "created_at >= ?", Date.parse(params[:start_on]).beginning_of_day )
+   end
+
+   if params[:end_on].present?
+     @registrations = @registrations.where( "created_at <= ?", Date.parse(params[:end_on]).end_of_day )
+   end
+
+
   end
 
   def destroy
